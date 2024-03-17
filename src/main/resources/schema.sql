56e099b70a2c1d365649b80cac4cab0a0ad83000
Vitor Lucas <vitorcrispim@yahoo.com.br>
2024-03-17 17:18:02 -0300

    Adicionando not null para a criação de tabelas

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS persons(
     id SERIAL primary key,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    participation decimal
+    first_name VARCHAR(255) NOT NULL ,
+    last_name VARCHAR(255) NOT NULL,
+    participation decimal NOT NULL
 );
